3b1f60f5012f3190a6da2a05e0489beceacbec15
unknown <jayeshjadhav@684@gmail.com>
2019-08-15 19:38:40 +0530

    Remove unecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-console.log('Client side javascript file')
+//console.log('Client side javascript file')
 
 // fetch('https://puzzle.mead.io/puzzle').then((response)=>{
 //     response.json().then((data)=>{
